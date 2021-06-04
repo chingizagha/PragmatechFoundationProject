c451ac1911a8764dd946ce9f28067ca5231d7f02
Chingiz Agha <cingiz167@gmail.com>
2021-06-04 10:56:35 +0400

    Project section added to admin panel

--- Back-end/Week10-Day01/app.py
+++ Back-end/Week10-Day01/app.py
@@ -1,4 +1,5 @@
 from logging import debug
+import re
 from werkzeug.datastructures import RequestCacheControl
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -23,13 +24,18 @@ class Blog(db.Model):
     def __repr__(self):
         return f'Blog: {self.title}'
 
+class Project(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String(20))
+
     
 
-@app.route('/')
+@app.route('/index')
 def index():
     blogs = Blog.query.all()
-
-
     return render_template('app/index.html', blogs=blogs) 
 
 @app.route('/about')
@@ -61,7 +67,10 @@ def single():
 def admin():
     return render_template('admin/admin.html')
 
-
+@app.route('/admin/blog')
+def blog_list():
+    blogs = Blog.query.all()
+    return render_template('admin/blog.html', blogs=blogs)
 
 @app.route('/admin/blog-add', methods=['GET', 'POST'])
 def blog_add():
@@ -76,18 +85,45 @@ def blog_add():
         )
         db.session.add(blog)
         db.session.commit()
-        return redirect(url_for('blog'))
+        return redirect(url_for('blog_list'))
     return render_template('admin/blog-add.html')
-
-@app.route('/admin/blog')
-def blog_list():
-    blogs = Blog.query.all()
-    return render_template('admin/blog.html', blogs=blogs)
 
 @app.route('/admin/blog-update')
 def blog_update():
     blogs = Blog.query.all()
     return render_template('admin/blog-update.html', blogs=blogs)
+    
+
+@app.route('/admin/project')
+def project_list():
+    projects = Project.query.all() 
+    return render_template('admin/project.html', projects=projects)
+
+@app.route('/admin/project-add', methods=['GET', 'POST'])
+def project_add():
+    if request.method=='POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        project = Project(
+            category = request.form['category'],
+            name = request.form['name'],
+            image = filename
+        )
+        db.session.add(project)
+        db.session.commit()
+        return redirect(url_for('project_list'))
+    return render_template('admin/project-add.html')
+
+@app.route('/admin/project-update')
+def project_update():
+    projects = Project.query.all()
+    return render_template('admin/project-update.html', projects=projects)
+
+
+
+
+
 
 
 if __name__ == "__main__":
